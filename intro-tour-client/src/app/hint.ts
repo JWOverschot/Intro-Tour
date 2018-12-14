491e0bee27c67f32db02ec3eaf712d868e8cfc17
Jis van Overschot <99040091@mydavinci.nl>
2018-12-14 20:46:25 +0100

    chnaged interfeces to classes

--- intro-tour-client/src/app/hint.ts
+++ intro-tour-client/src/app/hint.ts
@@ -1,7 +1,7 @@
-export interface Hint {
-    cost: number;
-    hint: string;
-    event_id: number;
-    id: number;
-    is_bougth: number;
+export class Hint {
+	cost: number;
+	hint: string;
+	event_id: number;
+	id: number;
+	is_bougth: number;
 }
